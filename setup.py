20e046b0a6703a81d0fdc4588eb7d54da9632930
Vivek singh <viveks04251@gmail.com>
2026-03-24 01:40:40 +0530

    setup and requirement file

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup 
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    requirement=[]
+    with open(file_path) as file_obj:
+        requirement=file_obj.readlines()
+        requirement=[req.replace("\n","") for req in requirement]
+        if '-e .' in requirement:
+            requirement.remove('-e .')
+
+setup(
+name="ML project",
+version='0.0.1',
+author='Vivek singh',
+author_email='viveks04251@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirement.txt')
+
+
+
+
+
+)
